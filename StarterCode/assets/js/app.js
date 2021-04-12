23c4e9f8edbd7247a1164ae2d1096ace40b3b107
Peter Johnson <74940976+pete6184@users.noreply.github.com>
2021-04-11 21:43:58 -0700

    setting up graph

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,17 @@
-// @TODO: YOUR CODE HERE!
+// Set SVG parameters
+const svgWidth = 900;
+const svgHeight = 500;
+
+// Set margins
+const margin = {
+    t: 20,
+    b: 20,
+    l: 20,
+    r: 20
+};
+
+const width = svgWidth - margin.left - margin.right;
+const height = svgHeigth - margin.top - margin.bottom;
+
+// Create SVG wrapper, append SVG group, shift margins
+const svg = d3.select("#scatter")
