b859ab87bf816dc31d40cd209702e4e1e264d336
Peter Johnson <74940976+pete6184@users.noreply.github.com>
2021-04-16 16:46:54 -0700

    checkpoint save

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -95,7 +95,7 @@ d3.csv("assets/data/data.csv").then(stateData => {
     // Create axes labels
     chartGroup.append('text')
         .attr('transform', 'rotate(-90)')
-        .attr('y', 0 - margin.left)
+        .attr('y', -5 - margin.left)
         .attr('x', 0 - (height / 1.3))
         .attr('dy', '1em')
         .attr('class', 'axisText')
